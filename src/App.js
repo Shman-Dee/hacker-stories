da5ef2986fbd1188af6eb24cbc94ed6e489d5417
Andrew McInally <realfunnyestate@yahoo.com>
2022-02-19 17:07:59 -0700

    built components

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import * as React from "react";
 
 const list = [
   {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
     num_comments: 3,
     points: 4,
-    objectID: 0
+    objectID: 0,
   },
   {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abromov, Andrew Clark',
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abromov, Andrew Clark",
     num_comments: 2,
     points: 5,
     objectID: 1,
@@ -24,31 +24,39 @@ function App() {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" placeholder="I am a placeholder"/>
-        {/*render the list here*/}
-        {/* and by the way: that's how you do comments in JSX */}
+      <Search />
+      
+      <hr />
 
-    <hr />
-
-      <ul>
-
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href="{item.url}">{item.title}</a>
-              </span>
-              <span> {item.author}</span>
-              <span> {item.num_comments}</span>
-              <span> {item.points}</span>
-            </li>
-          )
-        })}
-      </ul>
-    
+      <List />
     </div>
+  );
+}
 
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href="{item.url}">{item.title}</a>
+            </span>
+            <span> {item.author}</span>
+            <span> {item.num_comments}</span>
+            <span> {item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" placeholder="I am a placeholder" />
+    </div>
   );
 }
 
